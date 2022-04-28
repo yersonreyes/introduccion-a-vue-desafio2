8c5d9d4cd5ef4153b90b0f8c38cdfad44672d373
Yerson Reyes <yerson.programador@gmail.com>
2022-04-28 15:51:02 -0400

    proyecto terminado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-center mt-3" >Listado de tareas</h1>
+    <div class="container mt-5 mb-5">
+      <div class="input-group m-0">
+        <input  v-model="nuevaTarea" v-on:keypress.enter="agregarNuevaTarea" type="text" class="form-control" placeholder="Nueva tarea" >
+        <div class="input-group-append">
+        <button @click="agregarNuevaTarea" class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        </div>
+  	  </div>
+    </div>
+
+    <div class="container section2">
+      <ul class="list-group">
+        <li class="list-group-item" v-for="(tarea, $index) of tareas" :key="$index">
+        {{ $index + 1 }}: {{ tarea }}
+        <button  @click="eliminarTarea($index)" class="btn btn-danger ml-auto"> eliminar</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    nuevaTarea: "",
+    tareas: [],
+  }),
+    methods: {
+    agregarNuevaTarea() {
+      this.tareas.unshift(this.nuevaTarea);
+      this.nuevaTarea = "";
+    },
+    
+    eliminarTarea(eliminar){
+        if(eliminar == 0){
+          this.tareas.shift()
+        }else{
+          this.tareas.splice(eliminar,eliminar)
+        }
+      }
+    }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .section2{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+  .section2 ul{
+    width: 100%;
+  }
+
+ .list-group-item{
+    display: flex;
+  }
 </style>
